5bc0016a584b193b2ffdf10a83b320417d6e3e1f
muratboyar <muratboyar@gmail.com>
2018-04-25 09:10:02 +0300

    upd

--- c-c++/sysprog/main.c
+++ c-c++/sysprog/main.c
@@ -1,44 +1,15 @@
 ﻿/*
- * wait
+ * segfault
  */
 
-#define _GNU_SOURCE
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
+#include <stdio.h>
 
-int main(int argc, char **argv)
-{
-    pid_t pid;
-    char *env[] = { "CSD_City=Dublin", "CSD_Name=Cork", NULL };
-    if ((pid = fork()) < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+int main(int argc, char **argv){
 
-//    if (pid == 0 && execle("./sample", "./sample", "arg1", "arg2", "arg3", (char *)NULL, env) < 0) {
-//        perror("execle");
-//        exit(EXIT_FAILURE);
-//    }
+    char *ch = (char *)0x34756457;
 
-    char *args[] = {"./sample", "arg1", "arg2", "arg3", (char *)NULL};
-
-    if(pid == 0 && execvpe("./sample", args, env) < 0){
-//    if(pid == 0 && execvp("./sample", args) < 0){
-
-        perror("execle");
-        exit(EXIT_FAILURE);
-    }
-
-    int status;
-    wait(&status);
-
-    if(WIFEXITED(status))
-        printf("WEXITSTATUS: %d\n" , WEXITSTATUS(status));
-
-    printf("parent continues...\n");
+    putchar(*ch);
 
     return 0;
 }
