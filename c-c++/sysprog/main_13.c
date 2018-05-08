ad014a2551bf6e69ed5894bce13ae486f1f6e445
muratboyar <muratboyar@gmail.com>
2018-05-08 19:47:13 +0300

    new added shared mem

---
+++ c-c++/sysprog/main_13.c
@@ -0,0 +1,15 @@
+﻿/*
+ * segfault
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv){
+
+    char *ch = (char *)0x34756457;
+
+    putchar(*ch);
+
+    return 0;
+}
---
+++ c-c++/sysprog/main_14.c
@@ -0,0 +1,73 @@
+﻿///* process1.c */
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <fcntl.h>
+//#include <unistd.h>
+//#include <sys/stat.h>
+//#include <sys/mman.h>
+
+
+//void exit_sys(const char *msg);
+//int main(void)
+//{
+//    int fd;
+//    char *addr;
+//    char buf[4096 - 1];
+//    if ((fd = shm_open("/ThisIsATeest", O_CREAT|O_RDWR,
+//                       S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)) < 0)
+//        exit_sys("shm_open");
+//    if (ftruncate(fd, 4096) < 0)
+//        exit_sys("ftruncate");
+//    if ((addr = (char *)mmap(NULL, 4096, PROT_WRITE|PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED)
+//        exit_sys("mmap");
+//    printf("Enter text:");
+//    gets(buf);
+//    strcpy(addr, buf);
+//    printf("Press ENTER to exit!\n");
+//    getchar();
+//    munmap(addr, 4096);
+//    close(fd);
+//    return 0;
+//}
+//void exit_sys(const char *msg)
+//{
+//    perror(msg);
+//    exit(EXIT_FAILURE);
+//}
+
+/* process2.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+void exit_sys(const char *msg);
+int main(void)
+{
+    int fd;
+    char *addr;
+    if ((fd = shm_open("/ThisIsATeest", O_CREAT | O_RDWR,
+                       S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) < 0)
+        exit_sys("shm_open");
+    if (ftruncate(fd, 4096) < 0)
+        exit_sys("ftruncate");
+    if ((addr = (char *)mmap(NULL, 4096, PROT_READ, MAP_SHARED, fd, 0)) == MAP_FAILED)
+        exit_sys("ftruncate");
+    printf("Press ENTER to read from shared memory!\n");
+    getchar();
+    puts(addr);
+    printf("Press ENTER to exit!\n");
+    getchar();
+    munmap(addr, 4096);
+    close(fd);
+    return 0;
+}
+void exit_sys(const char *msg)
+{
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
