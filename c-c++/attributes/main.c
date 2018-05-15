ffe2c41506bfdd65b0709cafc6aef2d32df17ece
muratboyar <muratboyar@gmail.com>
2018-05-15 16:45:55 +0300

    minor

--- c-c++/attributes/main.c
+++ c-c++/attributes/main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
-int __attribute__((constructor)) x_init(void)
-{
-    puts("init() works");
-    return 0;
-}
+/*
+ *  thx to:
+ *      https://gcc.gnu.org/onlinedocs/gcc-4.7.0/gcc/Function-Attributes.html
+ *      https://stackoverflow.com/questions/2053029/how-exactly-does-attribute-constructor-work
+ *      https://askubuntu.com/questions/277288/dlclose-does-not-call-library-destructors-dlopen-called-just-once
+ */
 
-int __attribute__((destructor)) x_fini(void)
-{
-    puts("fini() works");
-    return 0;
-}
+void cConstructor(void);
+void cDestructor(void);
 
 int main()
 {
+    /*
+     * you can call con/dest c funcs seperatly
+     */
+    //x_fini();
     printf("Hello World!\n");
     return 0;
+}
+
+void __attribute__((constructor)) cConstructor(void)
+{
+    printf("%s is called\n", __FUNCTION__);
+}
+
+void __attribute__((destructor)) cDestructor(void)
+{
+    printf("%s is called\n", __FUNCTION__);
 }
